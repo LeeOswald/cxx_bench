c89b6678558f117b0c3a36428d225327de25edec
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-12-09 11:18:45 +0300

    benchmark std::function perf

--- calls.cpp
+++ calls.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <memory>
 
@@ -36,7 +37,7 @@ __attribute__((noinline)) void benchmark(A* a)
    Benchmark::Runner r;
 
    r.add(
-      "non-inline call",
+      "normal method",
       iterations,
       1,
       [iterations, a]()
@@ -48,7 +49,7 @@ __attribute__((noinline)) void benchmark(A* a)
    );
 
    r.add(
-      "virtual call",
+      "virtual method",
       iterations,
       1,
       [iterations, a]()
@@ -56,6 +57,80 @@ __attribute__((noinline)) void benchmark(A* a)
          auto c = iterations;
          while (c--)
             a->virtualCall(1);
+      }
+   );
+
+   std::function<void(std::size_t)> fun0 = 
+      std::bind(
+         &A::normalCall, 
+         a, 
+         std::placeholders::_1
+      );
+
+   r.add(
+      "std::function+bind -> method",
+      iterations,
+      1,
+      [iterations, &fun0]()
+      {
+         auto c = iterations;
+         while (c--)
+            fun0(1);
+      }
+   );
+
+   std::function<void(std::size_t)> fun1 =
+      std::bind(
+         &A::virtualCall,
+         a,
+         std::placeholders::_1
+      );
+
+   r.add(
+      "std::function+bind -> virtual method",
+      iterations,
+      1,
+      [iterations, &fun1]()
+      {
+         auto c = iterations;
+         while (c--)
+            fun1(1);
+      }
+   );
+
+   std::function<void(std::size_t)> lam0 = 
+   [a](std::size_t v)
+   {
+      a->normalCall(v);
+   };
+
+   r.add(
+      "std::function+lambda -> method",
+      iterations,
+      1,
+      [iterations, &lam0]()
+      {
+         auto c = iterations;
+         while (c--)
+            lam0(1);
+      }
+   );
+
+   std::function<void(std::size_t)> lam1 = 
+   [a](std::size_t v)
+   {
+      a->virtualCall(v);
+   };
+
+   r.add(
+      "std::function+lambda -> virtual method",
+      iterations,
+      1,
+      [iterations, &lam1]()
+      {
+         auto c = iterations;
+         while (c--)
+            lam1(1);
       }
    );
 

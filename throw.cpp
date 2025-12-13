c7db67ec561435e28e936de35c668fdaa9869319
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-12-13 11:59:27 +0300

    exception performance

---
+++ throw.cpp
@@ -0,0 +1,130 @@
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <vector>
+
+#include "benchmark.hpp"
+
+
+namespace
+{
+
+struct Bang
+{
+};
+
+
+std::size_t thrower(int bang, std::size_t v)
+{
+   if (bang > 0)
+      throw Bang{};
+
+   return v + bang;
+}
+
+std::size_t nothrowFor(
+   const std::vector<int>& bangs,
+   std::uint64_t iterations
+)
+{
+   std::size_t dummy = 0;
+   std::size_t current = 0;
+   std::size_t count = bangs.size();
+   while (iterations--)
+   {
+      dummy = thrower(bangs[current], dummy);
+
+      if (++current == count)
+         current = 0;
+   }
+
+   return dummy;
+}
+
+std::size_t throwFor(
+   const std::vector<int>& bangs,
+   std::uint64_t iterations
+)
+{
+   std::size_t dummy = 0;
+   std::size_t current = 0;
+   std::size_t count = bangs.size();
+   while (iterations--)
+   {
+      try
+      {
+
+         dummy = thrower(bangs[current], dummy);
+      }
+      catch (Bang&)
+      {
+      }
+      catch (...)
+      {
+         std::terminate();
+      }
+
+      if (++current == count)
+         current = 0;
+   }
+
+   return dummy;
+}
+
+
+
+} // namespace
+
+
+int main()
+{
+   constexpr std::uint64_t iterations = 1000000ULL;
+   Benchmark::Runner r("Exception performance", iterations);
+
+   volatile std::size_t dummy = 0;
+   std::vector<int> nobangs;
+   std::fill_n(
+      std::back_inserter(nobangs),
+      iterations,
+      0
+   );
+
+   r.add(
+      "baseline",
+      1,
+      [&nobangs, &dummy](std::uint64_t iterations)
+      {
+         dummy = nothrowFor(nobangs, iterations);
+      }
+   );
+
+   r.add(
+      "try+catch (xcpt not fired)",
+      1,
+      [&nobangs, &dummy](std::uint64_t iterations)
+      {
+         dummy = throwFor(nobangs, iterations);
+      }
+   );
+
+   std::vector<int> bangs;
+   std::fill_n(
+      std::back_inserter(bangs),
+      iterations,
+      1
+   );
+
+   r.add(
+      "try+throw+catch",
+      1,
+      [&bangs, &dummy](std::uint64_t iterations)
+      {
+         dummy = throwFor(bangs, iterations);
+      }
+   );
+
+
+   r.run(std::cout);
+
+   return 0;
+}
